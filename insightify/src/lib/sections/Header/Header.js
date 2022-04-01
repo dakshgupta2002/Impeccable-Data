8b93fe7e432705c7ca9422d92fe74c3a89144c09
dakshgupta2002 <dakshgupta2002@gmail.com>
2022-04-01 18:00:33 +0530

    header

--- insightify/src/lib/sections/Header/Header.js
+++ insightify/src/lib/sections/Header/Header.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './Header.css'
 
 export default function Header() {
     return (
-        <div className='header'>
-            <h6>Header</h6>
-            <p>Header Title</p>
+        <div className='head'>
+            <div className='title'>
+                Insightify
+            </div>
+
+            <div className='links'>
+                <Link to='/' className='link'>Home</Link>
+                <Link to='/about' className='link'>About</Link>
+                <Link to='/contact' className='link'>Contact</Link>
+            </div>
         </div>
     )
 }
