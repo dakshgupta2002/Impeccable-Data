fda909bef4b30cb210b2643abf6959727de95780
dakshgupta2002 <dakshgupta2002@gmail.com>
2022-04-01 16:22:01 +0530

    setup

---
+++ insightify/src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './styles.module.scss'
+
+export default function Home() {
+  return (
+    <div className={styles.parent}>
+        <div className={styles.child}>
+            <h1>Home</h1>
+        </div>
+    </div>
+  )
+}
