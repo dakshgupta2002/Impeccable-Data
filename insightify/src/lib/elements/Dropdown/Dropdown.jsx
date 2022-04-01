a35b8f39609ff7fea04e8649a79dbc7a36ba0ac1
dakshgupta2002 <dakshgupta2002@gmail.com>
2022-04-01 21:33:29 +0530

    dropdown

--- insightify/src/lib/elements/Dropdown/Dropdown.jsx
+++ insightify/src/lib/elements/Dropdown/Dropdown.jsx
@@ -6,10 +6,10 @@ export default function Dropdown({ items, value, onChange }) {
     const [isVisible, setIsVisible] = useState(false);
     return (
         <div className="dropdown">
-            <button className="dropbtn" onClick={() => {setIsVisible(!isVisible); console.log("hei")}}>
+            <button className="dropbtn" onClick={() => { setIsVisible(!isVisible); console.log("hei") }}>
                 {value}
             </button>
-
+            <div style={{height: '10px', width: '100%'}}></div>
             {isVisible && <div className="dropdown-items" >
                 {items.map((item) => {
                     if (item === value)
