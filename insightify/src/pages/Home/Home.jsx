e90e9a75d5bb95f8ae77d40bf3d81f1b93673228
dakshgupta2002 <dakshgupta2002@gmail.com>
2022-04-02 10:16:07 +0530

    great

--- insightify/src/pages/Home/Home.jsx
+++ insightify/src/pages/Home/Home.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Divider, Header } from '../../lib'
 import { Card } from '../../lib/elements/Card';
-import Dropdown from '../../lib/elements/Dropdown/Dropdown';
+// import Dropdown from '../../lib/elements/Dropdown/Dropdown';
 import './Home.css'
 
 export default function Home() {
-    const [month, setMonth] = useState('Jan');
+    // const [month, setMonth] = useState('Jan');
     return (
         <div className={'parent'}>
             <Header />
